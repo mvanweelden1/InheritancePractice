67e1e910f9c3b748837e5e3d0cc619e4176f121e
mvanweelden1@my.wctc.edu <>
2013-01-27 07:46:17 -0600

    Abstract package started

--- src/myabstract/Employee.java
+++ src/myabstract/Employee.java
@@ -5,5 +5,46 @@ package myabstract;
  * @author Mark Van Weelden
  */
 public abstract class Employee {
+    private String firstName;
+    private String lastName;
+    private int employeeNumber;
+
+    public Employee(String firstName, String lastName, int employeeNumber) {
+        setFirstName(firstName);
+        setLastName(lastName);
+        setEmployeeNumber(employeeNumber);
+        
+    }
     
+    public abstract double getGrossWeeklyPay();
+
+    public final void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public final void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public final void setEmployeeNumber(int employeeNumber) {
+        this.employeeNumber = employeeNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getEmployeeNumber() {
+        return employeeNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee[" + "First Name: " + firstName + ", Last Name: " +
+                lastName + ", Employee Number: " + employeeNumber + ']';
+    }
 }
